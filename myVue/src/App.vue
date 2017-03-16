a4dd75c3d276abb3f91833af1fb765f8fa1ca69e
linSkanda <anikiwayne@gmail.com>
2017-03-16 17:41:03 +0800

    updated added css and jquery

--- myVue/src/App.vue
+++ myVue/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+   <div class="container">
+      <div class="row">
+       <div id="content" class="col-lg-12 text-center" v-on:click="toSlideDiv">
+       </div>
+      </div>
+   </div>
     <hello>
     </hello>  
   </div>
@@ -7,9 +13,20 @@
 
 <script>
 import hello from './components/hello'
+import 'jquery/dist/jquery.js'
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default {
   name: 'app',
+  mounted () {
+    $('#content').append('<h1>hello</h1>')
+  },
+  methods: {
+    toSlideDiv: function () {
+      $('#content').addClass('animated bounceIn')
+    }
+  },
   components: {
     hello
   }
